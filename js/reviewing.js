5373beb67fe9f22ef85a51e4d256cfde854c008d
solenn-tl <solenn.tual@gmail.com>
2026-06-01 19:48:23 +0200

    Update review

--- js/reviewing.js
+++ js/reviewing.js
@@ -22,7 +22,7 @@
 
 const CVIS = {
   BOOK_LIST: ["Books"],
-  JOUR_LIST: ["MTAP","DSH"],
+  JOUR_LIST: ["MTAP","DSH","JOHD"],
   CONF_LIST: ["ICDAR","ESWC","APIA"],
   TIME_INTERVAL: ["2020", "2030"],
   SVG_W: 800,
